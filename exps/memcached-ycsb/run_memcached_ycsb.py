a01855ef8da08c604e29840f78c54b34b89c828f
Brent Stephens <brentstephens@cs.wisc.edu>
2017-03-01 21:10:49 -0600

    Adding in the Qdisc side of configuring the memcached rate-limiting experiment

--- exps/memcached-ycsb/run_memcached_ycsb.py
+++ exps/memcached-ycsb/run_memcached_ycsb.py
@@ -7,74 +7,31 @@ import platform
 import subprocess
 import yaml
 from time import sleep
-from start_memcached import *
 
 YCSB_DIR = '/scratch/bes/git/YCSB/'
+WORKLOAD = 'workload_read_asym'
+PROPERTIES = 'memcached_read_asym.properties'
 
-QMODEL_SQ = 'sq'
-QMODEL_MQ = 'mq'
-
-MEMCACHED_CONFIG_DEFAULTS = {
-    'qmodel': QMODEL_MQ,
-    'rate_limit': 5e9,
-    'workload': 'workload_read_asym',
-    'properties': 'memcached_read_asym.properties',
-    'servers': [
-        {'cpu': 0, 'port': 11212},
-        {'cpu': 4, 'port': 11214},
-    ],
-}
-
-class ServerConfig(object):
-    def __init__(self, *initial_data, **kwargs):
-        for dictionary in initial_data:
-            for key in dictionary:
-                setattr(self, key, dictionary[key])
-        for key in kwargs:
-            setattr(self, key, kwargs[key])
-    def dump(self):
-        return self.__dict__.copy()
-
-class MemcachedConfig(object):
-    def __init__(self, *initial_data, **kwargs):
-        for key in MEMCACHED_CONFIG_DEFAULTS:
-            setattr(self, key, MEMCACHED_CONFIG_DEFAULTS[key])
-        for dictionary in initial_data:
-            for key in dictionary:
-                if not hasattr(self, key):
-                    print 'WARNING! Unexpected attr: %s' % key
-                setattr(self, key, dictionary[key])
-        for key in kwargs:
-            if not hasattr(self, key):
-                print 'WARNING! Unexpected attr: %s' % key
-            setattr(self, key, kwargs[key])
-        # Parse the server config
-        self.servers = [ServerConfig(s) for s in self.servers]
-    def dump(self):
-        d = self.__dict__.copy()
-        d['servers'] = d['servers'].dump()
-        return d
-
-def memcached_ycsb_load(config):
+def memcached_ycsb_load():
     load_cmd_tmpl = 'cd %(ycsb_dir)s; ./bin/ycsb load memcached -s ' \
         '-P %(workload)s -P %(properties)s'
     load_cmd_args = {
         'ycsb_dir': YCSB_DIR, 
-        'workload': os.path.abspath(config.workload),
-        'properties': os.path.abspath(config.properties),
+        'workload': os.path.abspath(WORKLOAD),
+        'properties': os.path.abspath(PROPERTIES),
     }
     load_cmd = load_cmd_tmpl % load_cmd_args
     output = subprocess.check_call(load_cmd, shell=True)
     print 'Load output:'
     print output
 
-def memcached_ycsb_run(config):
+def memcached_ycsb_run():
     run_cmd_tmpl = 'cd %(ycsb_dir)s; ./bin/ycsb run memcached -s ' \
         '-P %(workload)s -P %(properties)s -threads 16'
     run_cmd_args = {
         'ycsb_dir': YCSB_DIR, 
-        'workload': os.path.abspath(config.workload),
-        'properties': os.path.abspath(config.properties),
+        'workload': os.path.abspath(WORKLOAD),
+        'properties': os.path.abspath(PROPERTIES),
     }
     run_cmd = run_cmd_tmpl % run_cmd_args
     output = subprocess.check_call(run_cmd, shell=True)
@@ -85,27 +42,17 @@ def main():
     # Parse arguments
     parser = argparse.ArgumentParser(description='Run YCSB for memcached for '
         'the rate-limiting and fairness experiment')
-    parser.add_argument('--config', help='An alternate configuration file. '
-        'The configuration format is unsurprisingly not documented.')
     args = parser.parse_args()
-
-    # Get the config
-    if args.config:
-        with open(args.config) as configf:
-            user_config = yaml.load(configf)
-        config = MemcachedConfig(user_config)
-    else:
-        config = MemcachedConfig()
 
     # Connect to the remote host, configure it, and start the memcached servers
     #TODO: This can be done with paramiko and the start_memcached.py script.
     # However, I'm leaving this until later for now.
 
     # YCSB: Load the database
-    memcached_ycsb_load(config)
+    memcached_ycsb_load()
 
     # YCSB: Run the benchmark
-    memcached_ycsb_run(config)
+    memcached_ycsb_run()
 
 if __name__ == '__main__':
     main()
--- exps/memcached-ycsb/start_memcached.py
+++ exps/memcached-ycsb/start_memcached.py
@@ -7,20 +7,131 @@ import platform
 import subprocess
 import yaml
 from time import sleep
-from run_memcached_ycsb import *
+
+QMODEL_SQ = 'sq'
+QMODEL_MQ = 'mq'
+
+MEMCACHED_CONFIG_DEFAULTS = {
+    'qmodel': QMODEL_SQ,
+    'rate_limit': 2e9,
+    'iface': 'p2p1',
+    'servers': [
+        {'cpu': 0, 'port': 11212},
+        {'cpu': 4, 'port': 11214},
+    ],
+}
+
+class ServerConfig(object):
+    def __init__(self, *initial_data, **kwargs):
+        for dictionary in initial_data:
+            for key in dictionary:
+                setattr(self, key, dictionary[key])
+        for key in kwargs:
+            setattr(self, key, kwargs[key])
+    def dump(self):
+        return self.__dict__.copy()
+
+class MemcachedConfig(object):
+    def __init__(self, *initial_data, **kwargs):
+        for key in MEMCACHED_CONFIG_DEFAULTS:
+            setattr(self, key, MEMCACHED_CONFIG_DEFAULTS[key])
+        for dictionary in initial_data:
+            for key in dictionary:
+                if not hasattr(self, key):
+                    print 'WARNING! Unexpected attr: %s' % key
+                setattr(self, key, dictionary[key])
+        for key in kwargs:
+            if not hasattr(self, key):
+                print 'WARNING! Unexpected attr: %s' % key
+            setattr(self, key, kwargs[key])
+        # Parse the server config
+        self.servers = [ServerConfig(s) for s in self.servers]
+    def dump(self):
+        d = self.__dict__.copy()
+        d['servers'] = d['servers'].dump()
+        return d
+
+def memcached_config_nic_driver(config):
+    # Remove the driver
+    rm_cmd = 'sudo rmmod ixgbe'
+    os.system(rm_cmd) # Ignore everything
+
+    # Craft the args for the driver
+    ixgbe = '../../code/ixgbe-5.0.4/src/ixgbe.ko'
+    rss_str = '' if config.qmodel == QMODEL_MQ else 'RSS=1,1'
+    drv_cmd = 'sudo insmod %s %s' % (ixgbe, rss_str)
+
+    # Add in the new driver
+    subprocess.check_call(drv_cmd, shell=True)
+
+    # Assign the IP
+    ip_cmd = 'sudo ifconfig %s 10.10.1.3 netmask 255.255.255.0' % config.iface
+    subprocess.check_call(ip_cmd, shell=True)
+
+def get_txqs(args):
+    txqs = glob.glob('/sys/class/net/%s/queues/tx-*' % args.iface)
+    return txqs
+
+def get_rxqs(config):
+    rxqs = glob.glob('/sys/class/net/%s/queues/rx-*' % config.iface)
+    return rxqs 
+
+def memcached_configure_rfs(config):
+    rxqs = get_rxqs(config)
+    entries = 65536
+    entries_per_rxq = entries / len(rxqs)
+    cmd = 'echo %d | sudo tee /proc/sys/net/core/rps_sock_flow_entries > /dev/null' % \
+        entries
+    subprocess.check_call(cmd, shell=True)
+    for rxq in rxqs:
+        cmd = 'echo %d | sudo tee /%s/rps_flow_cnt > /dev/null' % (entries_per_rxq, rxq)
+        subprocess.check_call(cmd, shell=True)
+
+def memcached_config_xps(config):
+    if config.qmodel == QMODEL_MQ:
+        # Use the Intel script to configure XPS
+        subprocess.call('sudo killall irqbalance', shell=True)
+        xps_script = '../../code/ixgbe-5.0.4/scripts/set_irq_affinity'
+        subprocess.call('sudo %s -x local %s' % (xps_script, config.iface),
+            shell=True)
+
+        # Also configure RFS
+        memcached_configure_rfs(config)
+
+def memcached_config_qdisc(config):
+    qcnt = len(get_txqs(config))
+    for i in xrange(1, qcnt + 1):
+        # Configure a tbf Qdisc per txq
+        rate_mbit = int(config.rate_limit / 1e6)
+        tc_cmd = 'sudo tc qdisc add dev %s parent :%x tbf rate %dmbit ' \
+            'burst 1000kb limit 10000000' % \
+            (config.iface, i, rate_mbit)
+        subprocess.check_call(tc_cmd, shell=True)
+
+        #XXX: This code doesn't work.  Also, htb is not necessary here.  tbf
+        # should work well enough instead.
+        # Default [x] means that unclassified traffic is sent to classid [x]
+        #tc_cmd = 'sudo tc qdisc add dev %s parent :%x handle %d0: htb default 12 r2q 1' % \
+        #    (config.iface, i, i)
+        #subprocess.check_call(tc_cmd, shell=True)
+        #tc_cmd = 'sudo tc class add dev %s parent %d0: classid %d0:12 htb rate 1000mbps' % \
+        #    (config.iface, i, i)
+        #subprocess.check_call(tc_cmd, shell=True)
 
 def memcached_config_server(config):
-    #TODO: Configure the number of NIC queues
-
-    #TODO: Configure Qdisc/TC
+    # Configure the number of NIC queues
+    memcached_config_nic_driver(config)
 
     #TODO: Configure XPS
-    pass
+    memcached_config_xps(config)
+
+    #TODO: Configure Qdisc/TC
+    memcached_config_qdisc(config)
 
 def memcached_start_servers(config):
     #XXX: "-L" enables hugepage support.  This currently didn't work for me.
-    #cmd_tmpl = '/usr/bin/memcached -m 1024 -p %(port)s -u memcache -t 1 -d -L'
-    cmd_tmpl = '/usr/bin/memcached -m 1024 -p %(port)s -u memcache -t 4 -d'
+    cmd_tmpl = '/usr/bin/memcached -m 4096 -p %(port)s -u memcache -t 8 -R 1000 -d -c 4096'
+
     procs = []
     for server_conf in config.servers:
         cmd = cmd_tmpl % {'port': server_conf.port}
@@ -54,6 +165,7 @@ def main():
 
     # Kill all old servers
     memcached_kill_servers(config)
+    sleep(0.1)
 
     # Configure the server
     memcached_config_server(config)
@@ -62,8 +174,8 @@ def main():
     procs = memcached_start_servers(config)
 
     #XXX: DEBUG
-    #for proc in procs:
-    #    print proc.stdout.read()
+    for proc in procs:
+        print proc.stdout.read()
 
 if __name__ == '__main__':
     main()
